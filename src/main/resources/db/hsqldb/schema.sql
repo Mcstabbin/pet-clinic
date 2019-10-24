1d8163f98209e851ecffad38572b733a9a12bbbd
Jacob McPhillips <jacob_mcphillips@intuit.com>
2019-10-24 10:09:19 -0700

    new contraint for unique visits for pet

--- src/main/resources/db/hsqldb/schema.sql
+++ src/main/resources/db/hsqldb/schema.sql
@@ -71,7 +71,8 @@ CREATE TABLE visits (
   description VARCHAR(255)
 );
 
-ALTER TABLE visits ADD CONSTRAINT unique_visits_times UNIQUE(vet_id, visit_date, time_id);
+ALTER TABLE visits ADD CONSTRAINT unique_visits_times_vet UNIQUE(vet_id, visit_date, time_id);
+ALTER TABLE visits ADD CONSTRAINT unique_visits_times_pet UNIQUE(pet_id, visit_date, time_id);
 ALTER TABLE visits ADD CONSTRAINT fk_visits_pets FOREIGN KEY (pet_id) REFERENCES pets (id);
 ALTER TABLE visits ADD CONSTRAINT fk_visits_time FOREIGN KEY (time_id) REFERENCES visit_times (id);
 ALTER TABLE visits ADD CONSTRAINT fk_visits_vets FOREIGN KEY (vet_id) REFERENCES vets (id);
